aa8ee49eb7ff97c860ab891b6e5d433007e0e148
Timmo2022 <timmomutumwa1@gmail.com>
2022-03-10 12:26:57 +0300

    a c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+
 /*
  * Here is our code
+ * Main is the entry point in this application
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
