89efb8cc54d44990d279412c205684111ba3b4e6
Timmo2022 <timmomutumwa1@gmail.com>
2022-03-10 12:39:53 +0300

    A c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /*
  * Here is our code
- * Main is the entry point in this application
  */
 
-int main(void) /*Mainnnn*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 
 }
