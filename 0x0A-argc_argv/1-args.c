5670cf14867de7bdf54c5a6a0963b6a1cd6770dd
Timmo <40892102+Timmox443@users.noreply.github.com>
2022-04-05 15:28:01 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - Entry point
  * @argc: count arguments passed to the program
  * @argv: array of pointers to strings
  * Return: 0 for success
  */
-
 int main(int argc, char *argv[]__attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
-}	
+}
