b0d1fb0c7386cd2d01af6212846cde7738a850cb
Timmo2022 <timmomutumwa1@gmail.com>
2022-04-05 17:29:17 +0300

    3 & 4

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that multiplies two numbers
+ * @argc:count of arguments passed to program
+ * @argv: array of pointers to strings
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+		printf("%d\n", atoi(argv[1] *atoi(argv[2]));
+	
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - program that adds positive numbers
+ * @argc:count of arguments passed to program
+ * @argv: array of pointers to strings
+ * Return:If no number is passed to the program,
+ * print 0, followed by a new line
+ * If one of the number contains symbols that are not digits,
+ * print Error, followed by a new line, and return 1
+ * 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
