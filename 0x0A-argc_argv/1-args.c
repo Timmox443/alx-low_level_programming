31918cbcbe0d3fa664aaae18537bc53c0b59795b
Timmo2022 <timmomutumwa1@gmail.com>
2022-04-05 15:22:32 +0300

    One

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: count arguments passed to the program
+ * @argv: array of pointers to strings
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[]__attribute__((un$)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}	
