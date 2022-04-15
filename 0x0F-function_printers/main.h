21a27c9262bb465f490119eb6ef8ee13c4e6f0a4
Timmo <40892102+Timmox443@users.noreply.github.com>
2022-04-15 21:35:53 +0300

    Delete 0x0F-function_printers directory

--- 0x0F-function_printers/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTION_POINTERS.H
-#define FUNCTION_POINTERS.H
-int _putchar(char c)
-void print_name(char *name, void (*f)(char *));
-int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
-
-
-#endif
